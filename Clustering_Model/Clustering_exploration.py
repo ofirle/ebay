8e1992905c175e04be6131a6bebf7b9000826578
Ido Finder <ido.finder@gmail.com>
2020-10-03 20:15:19 +0300

    update clustering model

--- Clustering_Model/Clustering_exploration.py
+++ Clustering_Model/Clustering_exploration.py
@@ -49,27 +49,44 @@ print('A vectorizer was created with {} tokens'.format(len(vectorizer.get_featur
 # create an input dataframe based on token's occurrences
 data_df = pd.DataFrame(data=X.toarray(), columns=vectorizer.get_feature_names())
 
-# train a clustering model
-kmeans = MiniBatchKMeans(random_state=12345)
+# # tune the number of clusters
+# list_k = range(2, 10)
+# sse = []
+# for k in list_k:
+#     km = MiniBatchKMeans(n_clusters=k)
+#     km.fit(data_df.values)
+#     sse.append(km.inertia_)
+#
+# # Plot sse against k
+# plt.figure(figsize=(6, 6))
+# plt.title('SSE based on Number of clusters')
+# plt.plot(list_k, sse, '-o')
+# plt.xlabel(r'Number of clusters *k*')
+# plt.ylabel('Sum of squared distance');
+# plt.show()
+
+# train a clustering model (with the chosen K)
+kmeans = MiniBatchKMeans(n_clusters=5, random_state=12345)
 kmean_indices = kmeans.fit_predict(data_df.values)
 print('\nClustering is finish: {} clusters were created'.format(len(set(kmean_indices))))
 
-# # Visualization - reducing to two dimentions using PCA
-# pca = PCA(n_components=2)
-# scatter_plot_points = pca.fit_transform(X.toarray())
-# colors = np.random.rand(len(set(kmean_indices)), 3)
-# x_axis = [o[0] for o in scatter_plot_points]
-# y_axis = [o[1] for o in scatter_plot_points]
-# fig, ax = plt.subplots(figsize=(20, 10))
-# plt.title('Listings Title Clustering')
-# ax.scatter(x_axis, y_axis, c=[colors[d] for d in kmean_indices])
-# for i, txt in enumerate(items.title.values):
-#     ax.annotate(txt, (x_axis[i], y_axis[i]))
-# plt.show()
+# Visualization - reducing to two dimentions using PCA
+pca = PCA(n_components=2)
+scatter_plot_points = pca.fit_transform(X.toarray())
+colors = np.random.rand(len(set(kmean_indices)), 3)
+x_axis = [o[0] for o in scatter_plot_points]
+y_axis = [o[1] for o in scatter_plot_points]
+fig, ax = plt.subplots(figsize=(20, 10))
+plt.title('Listings Title Clustering')
+ax.scatter(x_axis, y_axis, c=[colors[d] for d in kmean_indices])
+for i, txt in enumerate(items.title.values):
+    ax.annotate(txt, (x_axis[i], y_axis[i]))
+plt.show()
 
 # print clusters
 items['cluster'] = list(kmean_indices)
 
+print(kmeans.inertia_)
 for cluster in range(len(items['cluster'].unique())):
     print('\n----- Cluster {} -----'.format(cluster))
     cluster_titles = items[items['cluster'] == cluster].title.values
